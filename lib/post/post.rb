2558a156ee2b4b758535b4e6a55ffc3815c1622e
Cesar Navarro Araya <cnaraya01@gmail.com>
2013-07-24 00:20:00 -0600

    Modified post initialize for passing all the tests

--- lib/post/post.rb
+++ lib/post/post.rb
@@ -2,5 +2,8 @@ class Post
   attr_reader :title, :message, :author, :created_at
 
   def initialize(args={})
+    @title = args[:title]
+    @message = args[:message]
+    @author = args[:author]
   end
 end
